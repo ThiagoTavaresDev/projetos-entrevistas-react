710966ec0958ea6b94f8dcad35dfcffad081b2d5
Thiago Tavares <Thiagoff2019@hotmail.com>
2023-10-27 15:19:02 -0300

    atualizando

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import { Route, Routes } from 'react-router-dom'
 
 import { Home } from './pages/Home'
-import { Pagamento } from './pages/pagamento'
+import { Pagamento } from './pages/Pagamento'
 
 function App() {
  
